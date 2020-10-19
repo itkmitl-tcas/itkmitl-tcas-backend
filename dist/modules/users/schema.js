a86dbd7550bae5d24da89f93df55d1f200f22dea
Peaches <shinasaki07@gmail.com>
2020-10-19 22:22:18 +0700

    chore(*): ignore dist

--- dist/modules/users/schema.js
+++
@@ -1,6 +0,0 @@
-// import sql from '../../config/connection';
-// export const users = {
-//   create: sql('users/create.sql'),
-//   init: sql('users/init.sql'),
-// };
-//# sourceMappingURL=schema.js.map
