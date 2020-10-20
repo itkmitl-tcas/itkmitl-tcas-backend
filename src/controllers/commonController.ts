1b3b0e761dbd9230b8b4e2e592683ebcefae9a01
vasin sermsampan <dev.vasin@gmail.com>
2020-10-20 23:32:11 +0700

    fix

--- src/controllers/commonController.ts
+++ src/controllers/commonController.ts
@@ -1,11 +1,9 @@
-import { NextFunction, Request, Response } from 'express';
-import ServiceNotFoundException from '../exceptions/HttpException';
+import { Request, Response } from 'express';
 import { successResponse, unavailableResponse } from '../modules/common/service';
 
 export class CommonController {
-  public healthy(req: Request, res: Response, next: NextFunction) {
-    next(new ServiceNotFoundException(50, 'message'));
-    // successResponse('Healthy', null, res);
+  public healthy(req: Request, res: Response) {
+    successResponse('Healthy', null, res);
   }
 
   public unavaliable(req: Request, res: Response) {
