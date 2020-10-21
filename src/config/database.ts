69d9c5248eb3b438ed0d2336d3614b34f3c39899
vasin sermsampan <dev.vasin@gmail.com>
2020-10-21 23:55:02 +0700

    test db

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,6 @@ import { Sequelize } from 'sequelize';
 const connectString = `postgres://${env.getDBUsername()}:${env.getDBPassword()}@${env.getHost()}:${env.getDBPort()}/${
   env.getDBName() || ''
 }`;
-const sequelize = new Sequelize(connectString, { logging: false });
+const sequelize = new Sequelize(connectString, { logging: true });
 
 export const database = sequelize;
