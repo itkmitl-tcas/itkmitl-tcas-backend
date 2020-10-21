69712eaca43a54b62be67ba0063c6138c2d62b73
vasin sermsampan <dev.vasin@gmail.com>
2020-10-22 00:00:09 +0700

    chore(sequelize): enable log in production

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,6 @@ import { Sequelize } from 'sequelize';
 const connectString = `postgres://${env.getDBUsername()}:${env.getDBPassword()}@${env.getHost()}:${env.getDBPort()}/${
   env.getDBName() || ''
 }`;
-const sequelize = new Sequelize(connectString, { logging: true });
+const sequelize = new Sequelize(connectString, { logging: process.env.NODE_ENV == 'production' ? true : false });
 
 export const database = sequelize;
