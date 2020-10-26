d8e468bcae0cf3c8242645b2cb81ed68a7efa72a
vasin sermsampan <dev.vasin@gmail.com>
2020-10-26 16:04:33 +0700

    feat(docs): update docs
    - view pdf
    - create docs with upload pdf

--- src/routes/docs_routes.ts
+++ src/routes/docs_routes.ts
@@ -10,8 +10,12 @@ export class DocRoutes {
   public route(app: Application): void {
     app.route('/docs').get(this.docs_controller.healthy);
     app
+      .get('/docs/:type', AuthMiddelware(1), this.docs_controller.view)
+      .get('/docs/:type/:apply_id', AuthMiddelware(1), this.docs_controller.view)
       .post('/docs/get', AuthMiddelware(1), ValidationMiddleware(GetAllDocsDto, false), this.docs_controller.get)
       .post('/docs', AuthMiddelware(1), ValidationMiddleware(CreateDocsDto, false), this.docs_controller.create)
       .delete('/docs', AuthMiddelware(3), this.docs_controller.delete);
   }
 }
+
+// https://stackoverflow.com/questions/35847293/uploading-a-file-and-passing-a-additional-parameter-with-multer
