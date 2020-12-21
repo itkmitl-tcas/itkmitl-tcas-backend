ec183a555c41f9159852d3d14a59f5c5705612bc
vasin sermsampan <dev.vasin@gmail.com>
2020-12-21 21:47:00 +0700

    fix(user): disable email unique in user model

--- src/controllers/helper.ts
+++ src/controllers/helper.ts
@@ -1,8 +1,11 @@
 export function upsert(values: any, condition: any, model: any) {
-  return model.findOne({ where: condition }).then(function (obj: any) {
-    // update
-    if (obj) return obj.update(values);
-    // insert
-    return model.create(values);
-  });
+  return model
+    .findOne({ where: condition })
+    .then(function (obj: any) {
+      // update
+      if (obj) return obj.update(values);
+      // insert
+      return model.create(values);
+    })
+    .catch((err: any) => err);
 }
